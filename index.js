40f286654d1a08c58131933580c80e0a62e2d155
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-08-10 16:35:01 +0100

    eslint reconfig

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-
 const { Client, GatewayIntentBits } = require('discord.js');
-require("dotenv").config();
+require('dotenv').config();
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-	const { commandName } = interaction;
+  const { commandName } = interaction;
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
-	}
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'server') {
+    await interaction.reply('Server info.');
+  } else if (commandName === 'user') {
+    await interaction.reply('User info.');
+  }
 });
-
 
 // Login to Discord with your client's token
 client.login(DISCORD_TOKEN);
