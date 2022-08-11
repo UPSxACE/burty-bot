9dc07139badf8ea44bde3a59dd05428bacf6a202
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-08-11 13:55:52 +0100

    feature: warning for commands that cannot be executed manually

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -18,7 +18,11 @@ module.exports = {
       const command = message.client.commands.get(content[0]);
       if (content[0] !== '') {
         if (command) {
-          command.executeManual(message, content);
+          command.executeManual
+            ? command.executeManual(message, content)
+            : message.reply(
+                'This command cannot be executed manually. Execute it with the slashcommands feature.'
+              );
         } else {
           // If the command doesn't exist it doesn't need a reply, so I commented this line below
           // message.reply("That command doesn't seem to exist!");
