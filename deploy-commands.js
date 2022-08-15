7a5f405c87a1bf6f8b7f8c3468cd8a2f9240449b
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-08-15 13:26:12 +0100

    fix: deploy-commands properly working now, to setup global commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,7 +7,6 @@ require('dotenv').config();
 
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
-const SERVER_ID = process.env.SERVER_ID;
 
 // Read the commands folder, and add every .js file to a Collection object "client.commands"
 const commands = [];
@@ -25,16 +24,26 @@ for (const file of commandFiles) {
 const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN);
 
 // delete old
-/*
 rest
   .put(Routes.applicationCommands(CLIENT_ID), { body: [] })
   .then(() => console.log('Successfully deleted all application commands.'))
   .catch(console.error);
-*/
 
-// deploy new
+// deploy new to one server
+/*
+const SERVER_ID = process.env.SERVER_ID;
 rest
   .put(Routes.applicationGuildCommands(CLIENT_ID, SERVER_ID), {
+    headers: { Authorization: `Bot ${DISCORD_TOKEN}` },
+    body: commands,
+  })
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error);
+*/
+
+// deploy new to all servers
+rest
+  .put(Routes.applicationCommands(CLIENT_ID), {
     headers: { Authorization: `Bot ${DISCORD_TOKEN}` },
     body: commands,
   })
