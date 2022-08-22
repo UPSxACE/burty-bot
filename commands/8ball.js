eb790140a71b7f18e79ed06c84e2d3d977916063
Manuel Jorge Escaleira <a45433@alunos.ipb.pt>
2022-08-22 21:23:02 +0100

    feature:8ball

--- commands/8ball.js
+++ commands/8ball.js
@@ -7,22 +7,22 @@ module.exports = {
 	.addUserOption((option) =>
       option
         .setName('question')
+        .setDescription('something you\'d like to ask')
         .setRequired(false)
     ),
     async execute(interaction){
         const replies = [
-        "It is certain",
-		"It is decidedly so",
-		"Without a doubt",
-		"Yes – definitely",
-		"You may rely on it",
-		"As I see it",
-		"yes",
-		"Most Likely",
-		"Outlook good",
-		"Yes",
-		"Signs point to yes"];
+    'It is uncertain',
+		'It is decidedly so',
+		'Without a doubt',
+		'Yes definitely',
+		'You may rely on it',
+		'Yes',
+		'Most Likely No',
+		'Outlook good',
+		'No',
+		'Signs point to no'];
         const randomIndex = Math.floor(Math.random() * replies.lenght);
         interaction.reply(replies[randomIndex]);
     }
-		}
+}
